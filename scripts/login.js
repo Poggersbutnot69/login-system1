f03ac1169b31bfd779f624c7ecf2cdf82a4e9da0
jasperissupercool <106822439+Poggersbutnot69@users.noreply.github.com>
2022-09-21 10:47:12 -0500

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -3,7 +3,7 @@ function check(form) {
     password = new Array("admin1", "admin2", "members");
 
 	if(form.userid.value == username[0] && form.pswrd.value == password[0]||form.userid.value == username[1] && form.pswrd.value == password[1]) {
-		location.replace('#adminhome')/* The Administrator homepage */
+		location.replace('https://mmwmathfree29102818102817192819261947.netlify.app')/* The Administrator homepage */
 	}
 	else if(form.userid.value == username[2] && form.pswrd.value == password[2]) {
 		location.replace('members/home.html')/* The members homepage */
